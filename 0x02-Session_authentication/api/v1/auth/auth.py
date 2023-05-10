602d825ab5e50833f0364e5eac91731d547bfe78
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2023-05-10 23:16:22 +0300

    additional of a line

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ from flask import request
 from typing import List, TypeVar
 import os
 
+
 class Auth():
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
